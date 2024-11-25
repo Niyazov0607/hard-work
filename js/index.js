8f8891a94da3bdbb3416d9cc8378b20da4bde4fd
Niyazov0607 <shohjahonniyazov89@gmail.com>
2024-11-25 21:48:02 +0500

    hard work

---
+++ js/index.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            width: {
+                DEFAULT: "100%",
+                tablet:  "600px",
+                ltablet: "960px",
+                desktop: "1200px",
+            },
+            padding: {
+                DEFAULT: "20px",
+            },
+        },
+        screens: {
+            tablet: "640px",
+            ltablet: "1000px",
+            desktop: "1240px",
+        },
+    },
+};
